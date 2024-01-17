bd97d83554ff3a70f46b5613d03a862d86a9cd4a
Jaz-Lin <jaslin@berkeley.edu>
2024-01-17 11:23:31 -0800

    Updating lab01 skeleton

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /** Simple Arithmetic Class.
  * @author Josh Hug
  * */
@@ -11,7 +9,6 @@ public class Arithmetic {
      * @return Product of a and b
      * */
     public static int product(int a, int b) {
-        return a + b;
     }
 
     /** Computes sum of two ints (incorrectly).
@@ -24,11 +21,8 @@ public class Arithmetic {
     }
 
     public static void main(String[] args) {
-        Scanner  sc = new Scanner(System.in);
         System.out.println("Give me a number! (no decimals, please)");
-        int num1 = sc.nextInt();
         System.out.println("Give me another number! (still no decimals)");
-        int num2 = sc.nextInt();
 
         System.out.println("The product of " + num1 + " and " + num2 + " is: " + product(num1, num2));
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum(num1, num2));
